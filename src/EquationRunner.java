b9972cd68f1babdc10c9d48eb0f38b9cbaf50e31
liquancool <liquanl3@nycstudents.net>
2022-10-11 12:16:36 -0400

    Initial commit

---
+++ src/EquationRunner.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class EquationRunner {
+    public static void main(String[] args) {
+        String point1;
+        String point2;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        double x3;
+        Scanner s = new Scanner(System.in);
+        Scanner sDouble = new Scanner(System.in);
+        System.out.println("Welcome to the Linear Equation Calculator! What is your first coordinate point?");
+        point1 = s.nextLine();
+        System.out.println("Great! What is your second coordinate point?");
+        point2 = s.nextLine();
+        x1 = Integer.parseInt(point1.substring(1,2));
+
+    }
+}
